e36d821282b7e4f065e176b2d24e2601bdd37984
TimurKan74 <136612432+TimurKan74@users.noreply.github.com>
2023-08-28 19:47:02 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,27 +2,26 @@ const hamburgerButton = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav__links');
 const h1 = document.querySelector('h1');
 const navLinksAll = document.querySelectorAll('.nav__links a');
-console.log(navLinksAll);
+const iframe = document.querySelector('iframe');
+const wrapper = document.querySelector('.forum__wrapper');
 for (let i = 0; i < 4; i++) {
     navLinksAll[i].onclick = function () {
         hamburgerButton.classList.remove('hamburger_active')
         navLinks.classList.remove('nav__links_active');
     }
 }
-// const aboutLink = document.querySelector('.nav__links a:nth-child(1)');
-// const skilsLink = document.querySelector('.nav__links a:nth-child(2)');
-// skilsLink.onclick = function () {
-//     hamburgerButton.classList.remove('hamburger_active')
-//     navLinks.classList.remove('nav__links_active');
-// }
-// aboutLink.onclick = function () {
-//     hamburgerButton.classList.remove('hamburger_active')
-//     navLinks.classList.remove('nav__links_active');
-// }
 h1.onclick = function () {
     h1.classList.toggle('h1_active');
 }
 hamburgerButton.onclick = function () {
     hamburgerButton.classList.toggle('hamburger_active');
     navLinks.classList.toggle('nav__links_active');
+}
+
+iframe.onload = function () {//когда в айфрейм что то загружаеться, запускаться функция и 
+    wrapper.classList.add('wrapper_active');
+    setTimeout(() => {
+        document.forms.order.reset();
+        wrapper.classList.remove('wrapper_active');
+    }, 3000);
 }
